20cd4d5d9c702fb4743faf2185dea240fc93924a
dosmoc <dcesebiond@gmail.com>
2014-11-12 07:48:20 -0800

    exercises 2.29 - start of 2.37

--- 2_2_exercises.scm
+++ 2_2_exercises.scm
@@ -301,3 +301,429 @@
 ;this is what a list looks like (cons 1 (cons 2 (cons 3)))
 ;x is (cons (cons 1 (cons 2 nil)) (cons (cons 3 (cons 4 nil)) nil))
 
+;Exercise 2.29
+
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+;this is a tree with data at the nodes:
+
+
+;a)
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (car (cdr mobile)))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (car (cdr branch)))
+
+(define (count-leaves x)
+  (cond ((null? x) 0)  
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+;b)
+(define m1 
+  (make-mobile (make-branch 1 (make-mobile (make-branch 2 3) (make-branch 3 4))) (make-branch 5 6)))
+
+(define (weight? structure)
+  (not (pair? structure)))
+
+(define (identity x) x)
+
+(define (processor structure)
+  (if (weight? structure)
+      identity
+      total-weight))
+
+(define (total-weight mobile)
+   (define (branch-weights x)
+     (let ((structure (branch-structure x)))
+      (if (weight? structure)       
+         structure  
+         (+ (branch-weights (left-branch structure))
+            (branch-weights (right-branch structure))))))
+   
+   (+ (branch-weights (left-branch mobile))
+      (branch-weights (right-branch mobile))))
+
+(define (total-weight mobile)
+   (let ((left (left-branch mobile))
+         (right (right-branch mobile)))
+     (let ((lstruct (branch-structure left))
+           (rstruct (branch-structure right)))
+       (cond ((and (weight? lstruct) (weight? rstruct)) 
+              	(+ lstruct rstruct))
+             ((weight? lstruct) 
+              	(+ lstruct (total-weight rstruct)))
+             (else (+ rstruct (total-weight lstruct)))))))
+
+
+(define (total-weight mobile)
+   (let ((left (left-branch mobile))
+         (right (right-branch mobile)))
+     (let ((lstruct (branch-structure left))
+           (rstruct (branch-structure right)))
+       (+ ((processor lstruct) lstruct) ((processor rstruct) rstruct)))))
+
+;starting to see the need for type tagging... if you can tag as a mobile or struct, 
+;the program might start to be easier to understand
+
+(define m1 
+  (make-mobile (make-branch 1 (make-mobile (make-branch 2 3) (make-branch 3 4))) (make-branch 5 6)))
+
+;c)
+(define (balanced-mobile? mobile)
+   (define (balanced? wx lx wy ly)
+     (= (* wx lx) (* wy ly)))
+  
+   (let ((left (left-branch mobile))
+         (right (right-branch mobile)))
+     (let ((lstruct (branch-structure left))
+           (rstruct (branch-structure right))
+           (llength (branch-length right))
+           (rlength (branch-length right)))
+       (cond ((and (weight? lstruct) (weight? rstruct)) 
+              	(balanced? lstruct llength rstruct rlength))
+             ((weight? lstruct) 
+              	(and (balanced? lstruct llength (total-weight rstruct) rlength)
+                     (balanced-mobile? rstruct)))
+             (else (and (balanced? rstruct rlength (total-weight lstruct) llength)
+                        (balanced-mobile? lstruct)))))))
+
+(balanced-mobile? m1)
+(define m2 (make-mobile (make-branch 1 2) (make-branch 1 2)))
+(balanced-mobile? m2)
+;d)
+(define (make-mobile left right)
+  (cons left right))
+(define (make-branch length structure)
+  (cons length structure))
+
+;don't have to change much, just the selectors for right-branch and branch-structure
+(define (right-branch mobile)
+  (cdr mobile))
+
+(define (branch-structure branch)
+  (cdr branch))
+
+(define m1 
+  (make-mobile (make-branch 1 (make-mobile (make-branch 2 3) (make-branch 3 4))) (make-branch 5 6)))
+
+;Mapping over trees
+
+(define (scale-tree tree factor)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (* tree factor))
+        (else (cons (scale-tree (car tree) factor)
+                    (scale-tree (cdr tree) factor)))))
+(scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7))
+            10)
+
+(define (scale-tree tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (scale-tree sub-tree factor)
+             (* sub-tree factor)))
+       tree))
+
+;Exercise 2.30
+
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (* tree tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+;(1 (4 (9 16) 25) (36 49))
+
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree sub-tree)
+             (* sub-tree sub-tree)))
+       tree))
+
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+;(1 (4 (9 16) 25) (36 49))
+
+;Exercise 2.31.  
+(define (tree-map f tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map f sub-tree)
+             (f sub-tree)))
+       tree))
+
+(define (square-tree tree) (tree-map square tree))
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+
+;Exercise 2.32
+(define (subsets s)
+  (if (null? s)
+      (list nil)
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x) (append (list (car s)) x)) rest)))))
+
+;(subsets (list 1))
+;(rest (subsets '()))
+;(append (subsets '()) (map ? (subsets '())))
+;(append (nil) (map ? (nil)))
+;This is where I noticed that there's no way
+;to have 1 as part of the answer unless it's 
+;used in the map function
+;(append (nil) (map (lambda (x) (append (list (car (list 1)) x))) (nil)))
+;reduces to
+;(append (nil) (map (lambda (x) (append (1) x))) (list nil))
+;the map creates a list of appends of the car of s to the rest
+;(append (nil) ((append (1) nil))
+;(append (nil) ((1))
+;(() (1))
+;
+;you strip off the first item in the set and append it to the subsets
+;
+;(subsets (list 1 2))
+;(rest (subsets (list 2))
+;(append (subsets (list 2)) (map (lambda (x) (append (list (car s)) x)) (subsets (list 2)))
+
+(subsets (list 1 2 3))
+
+;2.2.3  Sequences as Conventional Interfaces
+;we need a definition of fib for these to work
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (* p p) (* q q))      ; compute p'
+                   (+ (* p q) (* q (+ p q)))      ; compute q'
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)  
+        ((not (pair? tree))
+         (if (odd? tree) (square tree) 0))
+        (else (+ (sum-odd-squares (car tree))
+                 (sum-odd-squares (cdr tree))))))
+
+(define (even-fibs n)
+  (define (next k)
+    (if (> k n)
+        nil
+        (let ((f (fib k)))
+          (if (even? f)
+              (cons f (next (+ k 1)))
+              (next (+ k 1))))))
+  (next 0))
+
+(map square (list 1 2 3 4 5))
+;(1 4 9 16 25)
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(filter odd? (list 1 2 3 4 5))
+;(1 3 5)
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+(accumulate + 0 (list 1 2 3 4 5))
+;15
+(accumulate * 1 (list 1 2 3 4 5))
+;120
+(accumulate cons nil (list 1 2 3 4 5))
+;(1 2 3 4 5)
+
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+(enumerate-interval 2 7)
+;(2 3 4 5 6 7)
+
+;To enumerate the leaves of a tree, we can use
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+(enumerate-tree (list 1 (list 2 (list 3 4)) 5))
+;(1 2 3 4 5)
+
+(define (sum-odd-squares tree)
+  (accumulate +
+              0
+              (map square
+                   (filter odd?
+                           (enumerate-tree tree)))))
+
+(define (even-fibs n)
+  (accumulate cons
+              nil
+              (filter even?
+                      (map fib
+                           (enumerate-interval 0 n)))))
+
+(define (list-fib-squares n)
+  (accumulate cons
+              nil
+              (map square
+                   (map fib
+                        (enumerate-interval 0 n)))))
+(list-fib-squares 10)
+;(0 1 1 4 9 25 64 169 441 1156 3025)
+
+(define (product-of-squares-of-odd-elements sequence)
+  (accumulate *
+              1
+              (map square
+                   (filter odd? sequence))))
+(product-of-squares-of-odd-elements (list 1 2 3 4 5))
+
+;Exercise 2.33.  
+;Fill in the missing expressions to complete the 
+;following definitions of some basic 
+;list-manipulation operations as accumulations:
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+
+;Exercise 2.34
+;Horener's rule structures computation as:
+;(...(AnX + An-1)X + ... + a1)X + A0
+;start with an, multiple by x, add an-1, multiply by x
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms) 
+                (+ this-coeff (* x higher-terms)))
+              0
+              coefficient-sequence))
+
+;For example, to compute 1 + 3x + 5x3 + x5 at x = 2 you
+;would evaluate
+(horner-eval 2 (list 1 3 0 5 0 1))
+
+(horner-eval 3 (list 1 3 0 5 0 1))
+
+(define x 3)
+(+ (* 1 (expt x 0)) (* 3 (expt x 1)) (* 0 (expt x 2)) (* 5 (expt x 3)) (* 0 (expt x 4)) (* 1 (expt x 5)))
+
+;Exercise 2.35.  Redefine count-leaves from section 2.2.2 as an accumulation:
+
+;original:
+(define (count-leaves x)
+  (cond ((null? x) 0)  
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+;accumulate:
+(define (inc x) (+ x 1))
+
+;count-leaves
+;works, but I don't think this is actually what 
+;they're looking for, but introduces a constant function
+(define (count-leaves t)
+  (accumulate +
+              0 (map (lambda (x) 1) (enumerate-tree t))))
+
+;this would work too, but doesn't use map like in the 
+;text:
+(define (count-leaves t)
+  (length (enumerate-tree t)))
+
+(count-leaves (cons (cons 1 2) 3))
+
+(length (enumerate-tree (list 1 2 (list 3 4) (list 5 6))))
+
+;you could do it this way, but doesn't seem as clear
+(define (count-leaves t)
+  (accumulate +
+              0 (map (lambda (sub-tree)
+				         (if (pair? sub-tree)
+				             (count-leaves sub-tree)
+				             1)) 
+                     t)))
+
+
+;Exercise 2.36.  
+;(define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12))) =>  
+;(accumulate-n + 0 s)
+;(22 26 30)
+
+(define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+  
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(accumulate-n + 0 s)
+
+;that was suprisingly cool; map is cool...so so cool
+
+;Exercise 2.37
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map <??> m))
+(define (transpose mat)
+  (accumulate-n <??> <??> mat))
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map <??> m)))
+
+(define v 
+  (list (list 1 2 3 4) 
+        (list 4 5 6 6) 
+        (list 6 7 8 9)))
