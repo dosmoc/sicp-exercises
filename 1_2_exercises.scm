d4e4fcdc1c21fd1b189b84c7ee71c8b40aaa665a
dosmoc <dcesebiond@gmail.com>
2014-11-14 07:42:16 -0800

    append exercises past section 1.2.6

--- 1_2_exercises.scm
+++ 1_2_exercises.scm
@@ -494,6 +494,8 @@
 ;(+ 0 1) (+ 0 1)
 ;(1 1)
 
+;1.2.5  Greatest Common Divisors
+
 ;Exercise 1.20
 
 (define (gcd a b)
@@ -566,3 +568,405 @@
 ;      2
 ;      (gcd 2 (remainder 2 2)))
 ;5 times
+
+
+
+;1.2.6  Example: Testing for Primality
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (prime? n)
+  (= n (smallest-divisor n))) 
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))  
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+; trying to understand this
+; the (fermat-test n) evaluates to a boolean, which means
+; in a cond, the consequent will be evaluated
+; which is a recursive call to fast-prime?
+; if ever (fermat-test n) is false, the procedure returns false --it does not return 
+; the results of the fermat-test directly because it needs to test multiple times
+; to reduce the probablility of error
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1))) ;if the fermat test is true, loop
+        (else false)))
+
+;Exercise 1.21.  Use the smallest-divisor procedure to find the smallest divisor of each 
+;of the following numbers: 199, 1999, 19999.
+(smallest-divisor 199)
+;199
+(smallest-divisor 1999)
+;1999
+(smallest-divisor 19999)
+;7 
+
+;Exercise 1.22
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes start limit)
+  (define (get-odd n)
+    (if (even? n) (+ n 1) n))
+
+  ;I don't even know why I needed a dummy call?
+  (define (search-it current found dummycall)
+    (cond ((= found limit) (newline))
+          ((prime? current) (search-it (+ current 2) (+ found 1) (timed-prime-test current)))
+          (else (search-it (+ current 2) found dummycall))))
+  (search-it (get-odd start) 0 (timed-prime-test start)))
+
+
+(search-for-primes 10000000000 3)
+;10000000019 *** .09999999999999432
+;10000000033 *** .10999999999999943
+;10000000061 *** .10999999999999943
+
+(search-for-primes 100000000000 3)
+;100000000003 *** .3500000000000014
+;100000000019 *** .3500000000000014
+;100000000057 *** .3400000000000034
+
+(search-for-primes 1000000000000 3)
+;1000000000039 *** 1.0700000000000003
+;1000000000061 *** 1.0799999999999983
+;1000000000063 *** 1.0799999999999983
+
+(search-for-primes 10000000000000 3)
+;10000000000037 *** 3.370000000000001
+;10000000000051 *** 3.3800000000000026
+;10000000000099 *** 3.3900000000000006
+
+;todo: answer the question
+
+;Exercise 1.23
+(define (smallest-divisor n)
+  (define (next x)
+    (if (= x 2) 3 (+ x 2)))
+  
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (next test-divisor)))))
+  
+  (find-divisor n 2))
+
+(search-for-primes 10000000000 3)
+;10000000019 *** .07000000000000028
+;10000000033 *** 6.0000000000002274e-2
+;10000000061 *** .07000000000000028
+
+(search-for-primes 100000000000 3)
+;100000000003 *** .23000000000000398
+;100000000019 *** .21999999999999886
+;100000000057 *** .21000000000000085
+ 
+(search-for-primes 1000000000000 3)
+;1000000000039 *** .6799999999999997
+;1000000000061 *** .6699999999999946
+;1000000000063 *** .6799999999999997
+ 
+(search-for-primes 10000000000000 3)
+;10000000000037 *** 2.1499999999999986
+;10000000000051 *** 2.1199999999999974
+;10000000000099 *** 2.1200000000000045
+
+;not quite half, more like 1.5 times as fast
+
+;Exercise 1.25
+
+;the substitution of fast-expt makes large deferred operations?
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+
+(define (expmod-ph base exp m)
+  (remainder (fast-expt base exp) m))
+
+;Exercise 1.26
+;this is tranformed to O(n) because the explicit multiplications
+;make this a tree recursion
+
+;is this a rule of thumb? iterative processes must only call
+;the procedure once to be iterative, as that makes another branch
+(define (expmod-lr base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (* (expmod-lr (/ exp 2) m)
+                       (expmod-lr (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod-lr (- exp 1) m))
+                    m))))
+
+;Exercise 1.27
+;carmichaels are: 561, 1105, 1729, 2465, 2821, 6601
+
+;Exercise 1.28
+
+;1.3  Formulating Abstractions with Higher-Order Procedures
+
+;1.3.1  Procedures as Arguments
+
+(define (sum-integers a b)
+  (if (> a b)
+      0
+      (+ a (sum-integers (+ a 1) b))))
+
+(define (pi-sum a b)
+  (if (> a b)
+      0
+      (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(sum-cubes 1 10)
+
+(define (identity x) x)
+
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(sum-integers 1 10)
+
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(* 8 (pi-sum 1 1000))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+(integral cube 0 1 0.01)
+
+(integral cube 0 1 0.001)
+
+;Exercise 1.29
+(define (integral-simpson f a b n)
+  (define h (/ (- b a ) n))
+    
+  (define (yk-next x)
+    (newline)
+    (display (* x 1.0))
+    (newline)
+    (+ x h)) 
+  (* (/ h 3.0)
+     (sum f a yk-next b)))
+
+(define (integral-simpson2 f a b n)
+  (define h (/ (- b a ) n))
+  
+  (define (seq a) (/ a h))
+  
+  (define (y-k x) (+ x h))
+  
+  (define (yk-next x)
+    (cond ((= a (seq x)) (y-k x))
+          ((= b (seq x)) (y-k x))
+          ((even? (seq x)) (* 2 (y-k x)))
+          (else (* 4 (y-k x)))))
+   
+  (* (/ h 3.0)
+     (sum f a yk-next b)))
+
+;the function is not applied
+;to each term directly
+;but in accordance with yk = f(a + kh)
+(define (integral-simpson3 f a b n)
+  (define h (/ (- b a ) n))
+  
+  (define (inc x)
+    (+ x 1))
+  
+  (define (y k)
+   (f (+ a (* k h))))
+  
+  (define (term k)
+   (* (cond ((odd? k) 4)
+            ((or (= k 0) (= k n)) 1)
+            ((even? k) 2))
+      (y k)))
+   
+  (* (/ h 3.0)
+     (sum term 0 inc n)))
+
+(integral cube 0 1 .001)
+
+(integral-simpson cube 0 1 1000)
+
+(integral-simpson2 cube 0 1 1000)
+
+(integral-simpson3 cube 0 1 1000)
+
+
+;Exercise 1.30
+(define (sum-itered term a next b)
+  (define (iter a result)
+    (newline)
+    (display a)
+    (display result)
+    
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+
+(sum-itered identity 2 inc 4)
+
+(sum-itered cube 2 inc 4)
+
+(sum-cubes 2 4)
+
+;Exercise 1.31
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* result (term a)))))
+  (iter a 1))
+
+
+;a.
+(define (factorial n)
+    
+  (define (next x) (+ x 1))
+  
+  (product identity 1 next n))
+
+(factorial 5)
+
+(define (pi-approximations n)
+  (define (next x) (+ x 1))
+  
+  (define (offset n) (+ n 2))
+
+  (define (numer n)
+    (if (or (= 0 n) (even? n))
+      (offset n)
+      (inc (offset n))))
+
+  (define (denom n)
+    (if (or (= 0 n) (even? n))
+      (inc (offset n))
+      (offset n)))
+
+  (define (term x)
+    (/ (numer x) (denom x)))
+  
+  (* 4.0 (product term 0 next n)))
+
+(factorial 1)
+(factorial 2)
+(factorial 3)
+(factorial 4)
+(factorial 5)
+
+;b.
+(define (product-recur term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product-recur term (next a) next b))))
+
+
+(pi-approximations 10000)
+
+(product identity 2 inc 5)
+
+(product-recur identity 2 inc 5)
+
+;Exercise 1.32
+;iterative
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+;recursive
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+
+
+(define (identity x) x)
+
+(define (inc x) (+ x 1))
+
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+
+;Exercise 1.33
+(define (filtered-accumulate combiner null-value term a next b filter)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+
+(define (filtered-accumulate combiner null-value term a next b filter)
+  ((cond ((> a b) null-value)
+         ((not (filter (term a)))
+          (combiner null-value
+                    (accumulate combiner null-value term (next a) next b)))
+         (else 
+          (combiner (term a)
+                    (accumulate combiner null-value term (next a) next b)))))
