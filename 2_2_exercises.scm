165d1c08fa73e59cc1c54cd9a7355df8ae162f10
dosmoc <dcesebiond@gmail.com>
2014-12-05 15:08:25 -0800

    set up for exercise 2.43

--- 2_2_exercises.scm
+++ 2_2_exercises.scm
@@ -886,3 +886,21 @@
     (= (+ (car x) (cadr x) (caddr x)) s))
   
   (filter sums? (unique-triples n)))
+
+
+;Exercise 2.42.  
+
+(define (queens board-size)
+  (define (queen-cols k)  
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
