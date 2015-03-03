388b1da7de5df75d65d11f12cc4d419c9b470459
dosmoc <dcesebiond@gmail.com>
2015-03-03 07:37:14 -0800

    reminder that 3.2 is all drawings

---
+++ 3_2_exercises.scm
@@ -0,0 +1,13 @@
+
+;3.2 The Environment Model of Evaluation
+;Drawing environment diagrams does not seem so fun,
+;but it'll help make sense of things
+
+;3.9 
+;Todo: Make a drawing
+
+;3.10 
+;Todo: Make more drawings
+
+;3.11 
+;Todo: Even more drawings!
