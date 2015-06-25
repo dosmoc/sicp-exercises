674ecd764a44b79647e69841b37aa1df6c387966
dosmoc <dcesebiond@gmail.com>
2015-06-25 15:20:39 -0700

    start symbolic algebra exercises

--- 2_5_exercises.scm
+++ 2_5_exercises.scm
@@ -829,8 +829,8 @@
           	 ;need to test if the op is dropable because
              ;it makes no sense to try to drop the results 
              ;of functions that map to non numeric values
-             (if (not (memq op dropable))
-                 (apply proc (map contents args))
+           (if (not (memq op dropable))
+               (apply proc (map contents args))
            		 (drop (apply proc (map contents args))))
         	 (if (not (all-same? type-tags))
             	 (apply apply-generic 
@@ -841,3 +841,73 @@
 ;(rational 1 . 4)
 (mul 1 (make-rational 2 2))
 ;(scheme-number 1))
+
+;Exercise 2.86 
+;Wow, maybe later
+
+;2.5.3  Example: Symbolic Algebra
+;Arithmetic on polynomials
+(define (add-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (add-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- ADD-POLY"
+             (list p1 p2))))
+(define (mul-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (mul-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- MUL-POLY"
+             (list p1 p2))))
+
+(define (install-polynomial-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))  
+  (define (variable? x) (symbol? x))
+
+
+  ;; representation of terms and term lists
+  <procedures adjoin-term ...coeff from text below>
+
+  ;; continued on next page
+
+  (define (add-poly p1 p2) ...)
+  <procedures used by add-poly>
+  (define (mul-poly p1 p2) ...)
+  <procedures used by mul-poly>
+  ;; interface to rest of the system
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial) 
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial) 
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  'done)
+
+(define (add-terms L1 L2)
+  (cond ((empty-termlist? L1) L2)
+        ((empty-termlist? L2) L1)
+        (else
+         (let ((t1 (first-term L1)) (t2 (first-term L2)))
+           (cond ((> (order t1) (order t2))
+                  (adjoin-term
+                   t1 (add-terms (rest-terms L1) L2)))
+                 ((< (order t1) (order t2))
+                  (adjoin-term
+                   t2 (add-terms L1 (rest-terms L2))))
+                 (else
+                  (adjoin-term
+                   (make-term (order t1)
+                              (add (coeff t1) (coeff t2)))
+                   (add-terms (rest-terms L1)
+                              (rest-terms L2)))))))))
+
