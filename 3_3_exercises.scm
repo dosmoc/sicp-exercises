68896591479519f8dbef0fa2a86c2b56530a8308
dosmoc <dcesebiond@gmail.com>
2015-06-25 15:23:53 -0700

    start chapter 3 exercises

--- 3_3_exercises.scm
+++ 3_3_exercises.scm
@@ -1053,3 +1053,94 @@ test-table
 	            (let ((result (f x)))
 	              (insert! x result table)
 	              result))))))
+
+;3.3.4 A Simulator for Digital Circuits
+
+;;For Section 3.3.4, used by and-gate
+;;Note: logical-and should test for valid signals, as logical-not does
+(define (logical-and x y)
+  (if (and (= x 1) (= y 1))
+      1
+      0))
+
+(define (logical-or x y)
+  (if (or (= x 1) (= y 1))
+      1
+      0))
+
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+;Exercise 3.28
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+;Exercise 3.29
+
+;needs to do this:
+;(define (o a b) (not (and (not a) (not b))))
+;(o #f #f) => #f
+;(o #t #t) => #t
+;(o #f #t) => #t
+;(o #t #f) => #t
+
+(define (or-gate a1 a2 output)
+  (let ((wa (make-wire))
+        (wb (make-wire))
+        (wc (make-wire))
+        (wd (make-wire)))
+    (not-gate a1 wa)
+    (not-gate a2 wb)
+    (and-gate wa wb wc)
+    (not-gate wc output)
+    'ok))
+
+;or-gate delay is (+ and-gate-delay (* 3 inverter-delay)
+
