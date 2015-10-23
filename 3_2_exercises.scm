82de28b2380dfaf7621f20355faaffa9d80272f6
dosmoc <dcesebiond@gmail.com>
2015-10-23 11:02:54 -0700

    add some drawings

--- 3_2_exercises.scm
+++ 3_2_exercises.scm
@@ -1,13 +1,45 @@
+;3.2 The Environmental Model of Evaluation
 
-;3.2 The Environment Model of Evaluation
-;Drawing environment diagrams does not seem so fun,
-;but it'll help make sense of things
+;3.2.2  Applying Simple Procedures
 
-;3.9 
-;Todo: Make a drawing
+;Exercise 3.9
 
-;3.10 
-;Todo: Make more drawings
+;Make sure to turn off word wrap for thiiiisss
+;       +---------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+;Global |                                                                                                                                                                     |
+;Env    |                                                                                                                                                                     |
+;       |                                                                                                                                                                     |
+;       |                                                                                                                                                                     |
+;       +----^----------------------------^----------------------------^----------------------------^----------------------------^----------------------------+---------------+
+;            |                            |                            |                            |                            |                            |
+;       +----+---+                   +----+---+                   +----+---+                   +----+---+                   +----+---+                   +----+---+
+;       |        |                   |        |                   |        |                   |        |                   |        |                   |        |
+;  E1+--> n : 6  |              E2+--> n : 5  |              E3+--> n : 4  |              E4+--> n : 3  |              E5+--> n : 2  |              E6+--> n : 1  |
+;       |        |                   |        |                   |        |                   |        |                   |        |                   |        |
+;       +--------+                   +--------+                   +--------+                   +--------+                   +--------+                   +--------+
+;
+; (if (= n 1)                  (if (= n 1)                  (if (= n 1)                  (if (= n 1)                  (if (= n 1)                  (if (= n 1)
+;   (* n (factorial (- n 1))))   (* n (factorial (- n 1))))   (* n (factorial (- n 1))))   (* n (factorial (- n 1))))   (* n (factorial (- n 1))))   (* n (factorial (- n 1))))
 
-;3.11 
-;Todo: Even more drawings!
+
+;Global +-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+;Env    |                                                                                                                                                                                                                                                             |
+;       |                                                                                                                                                                                                                                                             |
+;       |                                                                                                                                                                                                                                                             |
+;       +----^------------------------^----------------------------------^----------------------------------^----------------------------------^----------------------------------^----------------------------------^----------------------------------^-------------+
+;            |                        |                                  |                                  |                                  |                                  |                                  |                                  |
+;       +----+---+           +--------+------+                  +--------+------+                  +--------+------+                  +--------+------+                  +--------+------+                  +--------+------+                  +--------+------+
+;       |        |           |               |                  |               |                  |               |                  |               |                  |               |                  |               |                  |               |
+;  E1+--> n : 6  |      E2+--> product : 1   |             E3+--> product : 1   |             E4+--> product : 2   |             E5+--> product : 6   |             E6+--> product : 24  |             E7+--> product : 120 |             E8+--> product : 720 |
+;       |        |           | counter : 1   |                  | counter : 2   |                  | counter : 3   |                  | counter : 4   |                  | counter : 5   |                  | counter : 6   |                  | counter : 7   |
+;       +--------+           | max-count: 6  |                  | max-count: 6  |                  | max-count: 6  |                  | max-count: 6  |                  | max-count: 6  |                  | max-count: 6  |                  | max-count: 6  |
+;                            |               |                  |               |                  |               |                  |               |                  |               |                  |               |                  |               |
+;     (fact-iter 1 1 n)      +---------------+                  +---------------+                  +---------------+                  +---------------+                  +---------------+                  +---------------+                  +---------------+
+;
+;                       (if (> counter max-count)          (if (> counter max-count)          (if (> counter max-count)          (if (> counter max-count)          (if (> counter max-count)          (if (> counter max-count)          (if (> counter max-count)
+;                           product                            product                            product                            product                            product                            product                            product
+;                           (fact-iter (* counter product)     (fact-iter (* counter product)     (fact-iter (* counter product)     (fact-iter (* counter product)     (fact-iter (* counter product)     (fact-iter (* counter product)     (fact-iter (* counter product)
+;                                      (+ counter 1)                      (+ counter 1)                      (+ counter 1)                      (+ counter 1)                      (+ counter 1)                      (+ counter 1)                      (+ counter 1)
+;                                      max-count))                        max-count))                        max-count))                        max-count))                        max-count))                        max-count))                        max-count))
+
+;3.2.3  Frames as the Repository of Local State
