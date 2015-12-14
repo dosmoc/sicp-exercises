1793a395eac25fd6cc229c77026d79dad8b3425d
dosmoc <dcesebiond@gmail.com>
2015-12-14 15:09:03 -0800

    start exercises for 3.5

---
+++ 3_5_exercises.scm
@@ -0,0 +1,47 @@
+;3.5 Streams
+
+;3.5.1  Streams Are Delayed Lists
+
+(define (sum-primes a b)
+  (define (iter count accum)
+    (cond ((> count b) accum)
+          ((prime? count) (iter (+ count 1) (+ count accum)))
+          (else (iter (+ count 1) accum))))
+  (iter a 0))
+
+;here, enumerate-interval constructs the entire interval
+;at once before filtering
+(define (sum-primes a b)
+  (accumulate +
+              0
+              (filter prime? (enumerate-interval a b))))
+
+;by note 54
+;this are already defined in mit-scheme
+;(define the-empty-stream '())
+;(define (stream-null? s) (null? s))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
