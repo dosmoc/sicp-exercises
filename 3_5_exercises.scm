66a136892d97ab91e733ceb717e8588738040eb3
dosmoc <dcesebiond@gmail.com>
2016-06-02 21:52:04 -0700

    exercise 3.52

--- 3_5_exercises.scm
+++ 3_5_exercises.scm
@@ -212,3 +212,29 @@
 ;Interesting, is this because of memoization?
 (stream-ref x 3)
 ;Value: 3
+
+;Exercise 3.52
+(define sum 0)
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+(define y (stream-filter even? seq))
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0))
+                         seq))
+(stream-ref y 7)
+;7
+(display-stream z)
+;10
+;15
+;45
+;55
+;105
+;120
+;190
+;210
+;Value: done
+
+;sum is 210
+  
